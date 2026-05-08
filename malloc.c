c059dd72920560e6bd235e9a751ce13a94702aaa
Mahi Chandrawat <chandrawatmahi49@gmail.com>
2026-05-08 18:53:43 +0000

    Initial commit: my_malloc and my_free implementation

---
+++ malloc.c
@@ -0,0 +1,60 @@
+#define _GNU_SOURCE
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+struct block_header {
+    size_t size;
+    int is_free;
+    struct block_header *next;
+};
+
+struct block_header *free_list = NULL;
+
+void *my_malloc(size_t size) {
+
+  struct block_header *current = free_list;
+  while(current != NULL){
+    if(current->is_free && current->size >= size){
+      current->is_free = 0;
+      return (void *)(current + 1);
+    }
+    current = current->next;
+  }
+      size_t total = sizeof(struct block_header) + size;
+      void *block = sbrk(total);
+      if(block == (void *) - 1){
+        return NULL;  // malloc failed
+      }
+      struct block_header *header = (struct block_header *)block;
+      header->size = size;
+      header->is_free = 0;
+      header->next = NULL;
+
+      if (free_list == NULL) {
+        free_list = header;
+      } else{
+        struct block_header *temp = free_list;
+        while (temp->next != NULL){
+          temp = temp->next;
+        }
+        temp->next = header;
+      }
+      return (void *) (header + 1);
+    }
+
+    void my_free(void *p){
+      if (p == NULL) return;
+      struct block_header *header = (struct block_header *)p - 1;
+      header->is_free = 1;
+    }
+      
+  
+    int main() {
+      int *p = (int *)my_malloc(sizeof(int));
+      *p = 42;
+       printf("value:  %d\n", *p);
+       my_free(p);
+       printf("freed!\n");
+      return 0;
+    }
